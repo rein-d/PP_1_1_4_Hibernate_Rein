8c0979ec29f7afc80b345bf9c1b0d513eaa71052
rein_d <room083@gmail.com>
2022-10-11 00:31:02 +0300

    Task 1.1.3 - added rollback to previous statement in case of error

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -40,20 +40,17 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO USERS ( NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
+        String sql = "INSERT INTO USERS (NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
 
-        try (Connection connection = util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try (Connection connection = util.getConnection()) {
 
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-
-            int status = preparedStatement.executeUpdate();
-            if (status > 0) {
-                System.out.println("Successfully Inserted");
-            } else {
-                System.out.println("Insert Failed");
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
             }
             connection.commit();
         } catch (SQLException e) {
@@ -99,14 +96,13 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Connection connection = util.getConnection();
-             Statement statement = connection.createStatement()) {
+        try (Connection connection = util.getConnection()) {
 
-            int status = statement.executeUpdate("DELETE FROM USERS");
-            if (status > 0) {
-                System.out.println("Successfully cleaned");
-            } else {
-                System.out.println("Clean Failed");
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate("DELETE FROM USERS");
+            } catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
             }
             connection.commit();
         } catch (SQLException e) {
